c9354f53bbae564f5e58521026c6079c358684a6
hadpeter <hadpeter@umich.edu>
2023-05-17 17:36:59 -0400

    fix image path

--- components/home-page.tsx
+++ components/home-page.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage slug ="assets/pictures/home.JPEG" title={"About"} src={"/../public/assets/pictures/home.JPEG"}/>
+        <CoverImage slug ="assets/pictures/home.JPEG" title={"About"} src={"/assets/pictures/home.JPEG"}/>
       </div>
     </section>
   )

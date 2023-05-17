19ae331d05dda6a39c3241aeba8f55733b5bec8f
hadpeter <hadpeter@umich.edu>
2023-05-17 17:42:58 -0400

    updated loading title

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>{`Yoga: Hadley Peterson`}</title>
         </Head>
         <Container>
           <Intro />
